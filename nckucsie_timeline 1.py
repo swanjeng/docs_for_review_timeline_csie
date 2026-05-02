650d1dc5843fb429257eda1cc57749e866204f72
Swan Jeng <swanjeng1205@gmail.com>
2026-05-02 10:11:44 +0800

    Add files via upload

--- nckucsie_timeline 1.py
+++ nckucsie_timeline 1.py
@@ -15,10 +15,9 @@ dates = [date(1900, 1, 1), date(1905, 1, 1), date(1910, 1, 1)]
 min_date = date(np.min(dates).year - 5, np.min(dates).month, np.min(dates).day)
 max_date = date(np.max(dates).year + 5, np.max(dates).month, np.max(dates).day)
 
-label_dates = [date(1898, 1, 1), date(1902, 1, 1), date(1907, 7, 1)]
+label_dates = [date(1897, 7, 1), date(1902, 1, 1), date(1907, 1, 1)]
 
-str1 = """
-高中畢業 ~ 入學前：
+str1 = """高中畢業 ~ 入學前：
     1. 考七月的 APCS ，挑戰
        實作四級分以上。
     2. 運用台大開放式課程的
@@ -26,21 +25,18 @@ str1 = """
        代數和離散數學的先修。"""
 
 str2 = """大一：
-    1. 學習程式設計、數位電路導論、
-       微積分、線性代數等核心課程。
-    2. 選修「機器人軟體系統專案」、
-       通識課 「永續發展目標 (SDGs)
-       導論」等課程。
+    1. 學習程式設計、數位電路導論、微積分
+       、線性代數等核心課程。
+    2. 選修「機器人軟體系統專案」、通識課
+       「永續發展目標 (SDGs) 導論」等課程。
     3. 參與 電腦網路愛好社 社團。"""
 
 str3 = """大二：
-    1. 學習數位系統導論與實驗、
-       資料結構、離散數學、演算
-       法等核心課程。
-    2. 選修「Linux 系統與開源軟
-       體」、「視窗程式設計」、
-       「物聯網設備之程式設計與
-       界面」等課程。"""
+    1. 學習數位系統導論與實驗、資料結
+       構、離散數學、演算法等核心課程。
+    2. 選修「Linux 系統與開源軟體」、
+       「視窗程式設計」、「物聯網設備
+       之程式設計與界面」等課程。"""
 
 labels = [str1, str2, str3]
 
@@ -51,7 +47,7 @@ ax.axhline(0, xmin=0.05, xmax=0.95, color=(0.5, 0, 0), zorder=1, lw=3)
 
 ax.scatter(dates, np.zeros(len(dates)), s=240, color=(1, 0, 0), zorder=2)
 
-label_offsets = [0.3, -2.25, 0.3]
+label_offsets = [0.3, -1.8, 0.3]
 for i in range(len(dates)):
     ax.text(label_dates[i], label_offsets[i], labels[i], ha='left',
             color=(0, 0, 0), linespacing=2, backgroundcolor=(1, 0.9, 0.9))
--- nckucsie_timeline 2.py
+++ nckucsie_timeline 2.py
@@ -3,11 +3,11 @@ from datetime import date
 import numpy as np
 import matplotlib as mpl
 
-path = "/home/swan/Downloads/iansui/Iansui-Regular.ttf"
+path = "/home/swan/Downloads/LXGWWenKaiMonoTC-Regular.ttf"
 
 mpl.font_manager.fontManager.addfont(path)
 
-mpl.rcParams["font.family"] = "Iansui"
+mpl.rcParams["font.family"] = "LXGW WenKai Mono TC"
 mpl.rcParams["font.size"] = 28
 
 # 日期僅作為等間距設定用
@@ -18,23 +18,25 @@ max_date = date(np.max(dates).year + 5, np.max(dates).month, np.max(dates).day)
 label_dates = [date(1897, 1, 1), date(1902, 7, 1), date(1907, 7, 1)]
 
 str4 = """大三：
-    1. 學習微算機原理與應用、
-       作業系統、編譯系統等
-       核心課程。
-    2. 選修「多媒體系統與應
-       用」、「Linux 核心設
-       計」等課程。
-    3. 製作能夠幫助身障者的
-       資訊專題。"""
+    1. 學習微算機原理與應用、作業系統
+       、編譯系統等核心課程。
+    2. 選修「Linux 核心設計」、「多媒
+       體系統與應用」等課程。
+    3. 製作能夠幫助身障者的資訊專題。"""
 
 str5 = """大四：
-    1. 選修「人工智慧導論」和
+    1. 選修「人工智慧導論」、
        「影像處理、電腦視覺及
-       深度學習概論」等課程。"""
+       深度學習概論」和「創意
+       行動網路 APPs 之系統技
+       術與設計研發」等課程。"""
 
 str6 = """大學畢業後：
-    1. 繼續升學，前進資訊工程研究所。
-    2. 研究所畢業後，到科技公司就業。"""
+    1. 繼續升學，在資訊工程研究所學習
+       更深入的軟體工程及人工智慧知識。
+    2. 研究所畢業後，到科技公司就業，
+       程式開發時以使用者的需求為重。
+    3. 持續製作能夠幫助他人的程式作品。"""
 
 labels = [str4, str5, str6]
 
@@ -45,7 +47,7 @@ ax.axhline(0, xmin=0.05, xmax=0.95, color=(0.5, 0, 0), zorder=1, lw=3)
 
 ax.scatter(dates, np.zeros(len(dates)), s=240, color=(1, 0, 0), zorder=2)
 
-label_offsets = [-2.2, 0.3, -0.85]
+label_offsets = [-1.6, 0.3, -1.6]
 for i in range(len(dates)):
     ax.text(label_dates[i], label_offsets[i], labels[i], ha='left',
             color=(0, 0, 0), linespacing=2, backgroundcolor=(1, 0.9, 0.9))
